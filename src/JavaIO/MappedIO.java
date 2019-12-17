9b0f8e8ea8f78a41e320d3c736abac24a2d37d8d
龙打野 <18128966990@163.com>
2019-12-17 23:24:21 +0800

    P970

--- src/JavaIO/MappedIO.java
+++ src/JavaIO/MappedIO.java
@@ -1,35 +1,70 @@
 package JavaIO;
 
 import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
 import java.nio.IntBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
 
 public class MappedIO {
+
+    public static File toDirect(File f) throws IOException {
+        FileChannel fcIn = new FileInputStream(f).getChannel();
+        ByteBuffer b = ByteBuffer.allocateDirect((int) f.length());
+        fcIn.read(b);
+        b.flip();
+        Charset cs = Charset.defaultCharset();
+        CharBuffer cb = cs.decode(b);
+        String cbD1 = cb.toString().replace(".allocate(", ".allocateDirect(");
+        String cbD = cbD1.replace(f.getName().split("\\.")[0],
+                f.getName().split("\\.")[0] + "Direct");
+        CharBuffer cbNew = CharBuffer.wrap(cbD);
+        String ext = (f.getName().split("\\."))[1];
+        String outFile = (f.getName().split("\\."))[0] +
+                "Direct." + ext;
+        FileChannel fcOut = new FileOutputStream(new File(outFile)).getChannel();
+        fcOut.write(cs.encode(cbNew));
+        // fcIn.transferTo(0, f.length(), fcOut);
+        fcIn.close();
+        fcOut.close();
+        return new File(outFile);
+    }
+
+
+
     private static int numOfInts = 4000000;
     private static int numOfUbuffInts = 200000;
+
     private abstract static class Tester {
         private String name;
-        public Tester(String name) { this.name = name; }
+
+        public Tester(String name) {
+            this.name = name;
+        }
+
         public void runTest() {
             System.out.print(name + ": ");
             try {
                 long start = System.nanoTime();
                 test();
                 double duration = System.nanoTime() - start;
-                System.out.format("%.2f\n", duration/1.0e9);
-            } catch(IOException e) {
+                System.out.format("%.2f\n", duration / 1.0e9);
+            } catch (IOException e) {
                 throw new RuntimeException(e);
             }
         }
+
         public abstract void test() throws IOException;
     }
+
     private static Tester[] tests = {
             new Tester("Stream Write") {
                 public void test() throws IOException {
                     DataOutputStream dos = new DataOutputStream(
                             new BufferedOutputStream(
                                     new FileOutputStream(new File("temp.tmp"))));
-                    for(int i = 0; i < numOfInts; i++)
+                    for (int i = 0; i < numOfInts; i++)
                         dos.writeInt(i);
                     dos.close();
                 }
@@ -42,7 +77,7 @@ public class MappedIO {
                     IntBuffer ib = fc.map(
                             FileChannel.MapMode.READ_WRITE, 0, fc.size())
                             .asIntBuffer();
-                    for(int i = 0; i < numOfInts; i++)
+                    for (int i = 0; i < numOfInts; i++)
                         ib.put(i);
                     fc.close();
                 }
@@ -52,7 +87,7 @@ public class MappedIO {
                     DataInputStream dis = new DataInputStream(
                             new BufferedInputStream(
                                     new FileInputStream("temp.tmp")));
-                    for(int i = 0; i < numOfInts; i++)
+                    for (int i = 0; i < numOfInts; i++)
                         dis.readInt();
                     dis.close();
                 }
@@ -64,7 +99,7 @@ public class MappedIO {
                     IntBuffer ib = fc.map(
                             FileChannel.MapMode.READ_ONLY, 0, fc.size())
                             .asIntBuffer();
-                    while(ib.hasRemaining())
+                    while (ib.hasRemaining())
                         ib.get();
                     fc.close();
                 }
@@ -74,7 +109,7 @@ public class MappedIO {
                     RandomAccessFile raf = new RandomAccessFile(
                             new File("temp.tmp"), "rw");
                     raf.writeInt(1);
-                    for(int i = 0; i < numOfUbuffInts; i++) {
+                    for (int i = 0; i < numOfUbuffInts; i++) {
                         raf.seek(raf.length() - 4);
                         raf.writeInt(raf.readInt());
                     }
@@ -89,14 +124,15 @@ public class MappedIO {
                             FileChannel.MapMode.READ_WRITE, 0, fc.size())
                             .asIntBuffer();
                     ib.put(0);
-                    for(int i = 1; i < numOfUbuffInts; i++)
+                    for (int i = 1; i < numOfUbuffInts; i++)
                         ib.put(ib.get(i - 1));
                     fc.close();
                 }
             }
     };
+
     public static void main(String[] args) {
-        for(Tester test : tests)
+        for (Tester test : tests)
             test.runTest();
     }
 }
